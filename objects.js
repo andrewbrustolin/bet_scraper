15c89a1e0774acb12a9e65942dc69b1b023ceb0f
Andrew Brustolin <andrewbrustolin@gmail.com>
2022-08-13 14:41:59 -0300

    xpathgroupsetter and xpathassembler functions added

--- objects.js
+++ objects.js
@@ -1,126 +1,245 @@
+const houses_obj = {
+    houses: [
+        {
+            name: "bet365",
+            url: "https://www.bet365.com/#/AC/B1/C1/D1002/E71022033/G40/",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+            
 
-const houses = [
-    {
-        nome: "bet365",
-        url: "https://www.bet365.com/",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-          
+           
+            
+            
+
+        },
+        {
+            name: "Betano",
+            url: "https://br.betano.com/",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Betfair",
+            url: "https://www.betfair.com/br",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "1xBet",
+            url: "https://br.1xbet.com/pt",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Betmotion",
+            url: "https://www.betmotion.com/br/S",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Dafabet",
+            url: "https://www.dafabet.com/pt",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Betsson",
+            url: "https://www.betsson.com/br",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Betway",
+            url: "https://betway.com/pt/sports",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "Sportsbet.io",
+            url: "https://sportsbet.io/pt/sports",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        },
+        {
+            name: "LeoVegas",             
+            url: "https://www.leovegas.com/pt-br/",
+            parent_xpath: "/html/body/div[1]/div/div[3]/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div",
+            id_group: {
+                date_dom_id: "rcl-MarketHeaderLabel rcl-MarketHeaderLabel-isdate",
+                home_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                away_team_dom_id: "rcl-ParticipantFixtureDetailsTeam_TeamName",
+                home_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                draw_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds",
+                away_odds_dom_id: "sgl-ParticipantOddsOnly80_Odds"
+            },
+            xpath_group: [],
+            house_events: []
+        }
+    ],
+
+    //console.log(Object.values(houses[0].xpath_group));
+    //Constructor
+
+    eventConstruct: function(date, home_team, away_team, home_odds, draw_odds, away_odds){
+
+        this.date = date;
+        this.home_team = home_team;
+        this.away_team = away_team;
+        this.home_odds = home_odds;
+        this.draw_odds = draw_odds;
+        this.away_odds = away_odds;
+
     },
-    {
-        nome: "Betano",
-        url: "https://br.betano.com/",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
+
+
+    /*xpathGroupSetter: function() {
+
+        this.houses.forEach(function(parent) {
+            parent.id_group.forEach(function(child) {
+                console.log(child);
+            });    
+            
+        });
+
+    },*/
+
+
+    xpathAssembler: function(par_xpath, class_id) {
+
+        return (par_xpath + "[@class='" + class_id + "']");
+        
     },
-    {
-        nome: "Betfair",
-        url: "https://www.betfair.com/br",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "1xBet",
-        url: "https://br.1xbet.com/pt",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "Betmotion",
-        url: "https://www.betmotion.com/br/S",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "Dafabet",
-        url: "https://www.dafabet.com/pt",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "Betsson",
-        url: "https://www.betsson.com/br",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "Betway",
-        url: "https://betway.com/pt/sports",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "Sportsbet.io",
-        url: "https://sportsbet.io/pt/sports",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
-    },
-    {
-        nome: "LeoVegas",             
-        url: "https://www.leovegas.com/pt-br/",
-        date_dom_id: "",
-        home_team_dom_id: "",
-        away_team_dom_id: "",
-        home_odds_dom_id: "",
-        draw_odds_dom_id: "",
-        away_odds_dom_id: "",
-        house_events: []
+
+    xpathGroupSetter: function() {
+
+         for(i = 0; i < this.houses.length; i++){
+            for(j = 0; j < Object.keys(this.houses[i].id_group).length; j++){
+
+                this.houses[i].xpath_group[j] = this.xpathAssembler(this.houses[i].parent_xpath, Object.values(this.houses[i].id_group)[j]);
+                //console.log(Object.values(this.houses[i].id_group)[j]);
+                console.log(this.houses[i].xpath_group[j]);
+
+            }
+            
+            
+        }
     }
-];
 
-const event = {
+    //const event = [];
+}
 
-    date: "",
-    home_team: "",
-    away_team: "",
-    home_odds: "",
-    draw_odds: "",
-    away_odds: ""
+houses_obj.xpathGroupSetter();
 
-};
- 
+
+/*
+
+for(n = 0; n < houses.length; n++){
+
+
+
+
+
+
+    while(event.date = "13 agosto"){
+
+        let event[i] = new event_construct(date[x], home_team[x], away_team[x], home_odds[x], draw_odds[x], away_odds[x]);
+
+        houses[n].house_events.push(event[i]);
+
+
+
+    }
+}
+
+
+
+for(index = 1; index <= date.length; index++){
+
+
+
+}
+
+
+*/
+
